df23275ad42ee8e0dbc1381324848ee7079998a2
Unknown <paulina.hertor@gmail.com>
2018-02-22 14:00:45 +0100

    adddBtn-function

--- www/js/classes/form.class.js
+++ www/js/classes/form.class.js
@@ -3,35 +3,68 @@ class Form extends Base{
 	constructor(){
 		super();
 		this.render('main .container');
-
+		this.clickBtnAuther();
 	}
+	
+
+	clickBtnAuther(){
+		$(document).on('click', '.btn', function(){
+				let bookAuthor = $('#filter-author').val();
+				$.ajax({
+					type: 'GET',
+					url: `/books/author/${bookAuthor}`,
+					dataType: 'json',
+					success: (books) => {
+						// render books
+						for (let book of books) {
+							$('.result').append(`
+								<h1 class="text-center mb-3">Your book</h1>
+								<ul>
+									<li class="title">Title: ${book.title}</li>
+									<li class="title">Author: ${book.author}</li>
+									<li class="title">Country: ${book.country}</li>
+									<li class="title">Language: ${book.language}</li>
+									<li class="title">Pages: ${book.pages}</li>
+									<li class="title">Year: ${book.year}</li>
+								</ul>
+								`);
+							 $('#filter-author').val('');
+							console.log(book);
+						}
+					}
+				});
+			})
+		}
+
+	clickBtnTitle(){
+		$(document).on('click', '.btn', function(){
+				let bookAuthor = $('#filter-title').val();
+				$.ajax({
+					type: 'GET',
+					url: `/books/title/${bookTitle}`,
+					dataType: 'json',
+					success: (books) => {
+						// render books
+						for (let book of books) {
+							$('.result').append(`
+								<h1 class="text-center mb-3">Your book</h1>
+								<ul>
+									<li class="title">Title: ${book.title}</li>
+									<li class="title">Author: ${book.author}</li>
+									<li class="title">Country: ${book.country}</li>
+									<li class="title">Language: ${book.language}</li>
+									<li class="title">Pages: ${book.pages}</li>
+									<li class="title">Year: ${book.year}</li>
+								</ul>
+								`);
+							 $('#filter-title').val('');
+							console.log(book);
+						}
+					}
+				});
+			})
+		}
 
 
-	click(){
-		let bookAuthor = $('#filter-author').val();
-		$.ajax({
-			type: 'GET',
-			url: `/books/author/${bookAuthor}`,
-			dataType: 'json',
-			success: (books) => {
-				// render books
-
-				for (let book of books) {
-					$('.result').append(`
-						<h1 class="text-center mb-3">Your book</h1>
-						<ul>
-							<li class="title">Title: ${book.title}</li>
-							<li class="title">Author: ${book.author}</li>
-							<li class="title">Country: ${book.country}</li>
-							<li class="title">Language: ${book.language}</li>
-							<li class="title">Pages: ${book.pages}</li>
-							<li class="title">Year: ${book.year}</li>
-						</ul>
-						`);
-					console.log(book);
-				}
-
-			}
-		});
-	}
 }
+
