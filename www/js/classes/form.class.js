ee424242a51f5a24726e60804932a8e687c04f74
Shamin Krook <shahmineh.gh@hotmail.com>
2018-02-22 13:33:47 +0100

    Can search a book from author

--- www/js/classes/form.class.js
+++ www/js/classes/form.class.js
@@ -17,7 +17,17 @@ class Form extends Base{
 				// render books
 
 				for (let book of books) {
-					$('.result').append(book.author);
+					$('.result').append(`
+						<h1 class="text-center mb-3">Your book</h1>
+						<ul>
+							<li class="title">Title: ${book.title}</li>
+							<li class="title">Author: ${book.author}</li>
+							<li class="title">Country: ${book.country}</li>
+							<li class="title">Language: ${book.language}</li>
+							<li class="title">Pages: ${book.pages}</li>
+							<li class="title">Year: ${book.year}</li>
+						</ul>
+						`);
 					console.log(book);
 				}
 
